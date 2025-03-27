3cf3d3b0f72e1f05f9288f2e6fad7136d9184a0d
Kevin <kevin.desa@gmail.com>
2025-03-27 15:26:46 +1100

    Update android_build_env.sh

--- .circleci/android_build_env.sh
+++ .circleci/android_build_env.sh
@@ -47,9 +47,9 @@ DEBIAN_FRONTEND=noninteractive \
     ${PACKAGES} -y
 
 echo -e "Installing GitHub CLI"
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
 apt install -y gh
 
