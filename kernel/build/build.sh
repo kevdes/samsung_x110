1ebd992579f2053804328f6d226084d7f6c3778f
Kevin <kevin.desa@gmail.com>
2025-04-01 17:13:02 +1100

    Update build.sh

--- kernel/build/build.sh
+++ kernel/build/build.sh
@@ -557,9 +557,10 @@ CC_ARG="${CC}"
 #cd ${ROOT_DIR}
 #cd ${KERNEL_DIR}
 
-source "${ROOT_DIR}/build/_setup_env.sh"
+# source "${ROOT_DIR}/build/_setup_env.sh"
 
-cd ${ROOT_DIR}/kernel-5.10
+cd ../kernel-5.10
+echo pwd
 make LLVM=1 LLVM_IAS=1 DEPMOD=depmod DTC=dtc O=/root/project/predator/samsung/out/target/product/gta9wifi/obj/KERNEL_OBJ/kernel-5.10 gki_defconfig 
 make LLVM=1 LLVM_IAS=1 DEPMOD=depmod DTC=dtc O=/root/project/predator/samsung/out/target/product/gta9wifi/obj/KERNEL_OBJ/kernel-5.10
 
